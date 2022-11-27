3d392bfab4675e3b05c8a753e0c6e56a109f2e22
Joao Vitor Mocambite Lima <86741407+JoaoVitor2022dev@users.noreply.github.com>
2022-11-27 01:35:25 -0400

    Update App.js

--- secretword/src/App.js
+++ secretword/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import StartScreen from './components/StartScreen';
+
 function App() {
   return (
     <div className="App">
-      <h2>Secrete Word</h2>  
+      <StartScreen/>
     </div>
   );
 }
