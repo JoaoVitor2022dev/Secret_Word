ad20b226e542f00c77e8804afd0928a133c7a3f7
Joao Vitor Mocambite Lima <86741407+JoaoVitor2022dev@users.noreply.github.com>
2022-11-27 01:33:22 -0400

    Add files via upload

---
+++ secretword/src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+        <h1>Secret word</h1>
+        <p>clique no botao abaixo para comecar a jogar</p>
+        <button>Comecar o jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen; 
