7898d4c207f39145664d072599e2f9babc9b9830
Joao Vitor Mocambite Lima <86741407+JoaoVitor2022dev@users.noreply.github.com>
2022-12-07 17:34:13 -0400

    Update Game.js

--- secretword/src/components/Game.js
+++ secretword/src/components/Game.js
@@ -2,9 +2,30 @@ import './Game.css';
 
 const Game = ( { verifyLetter }) => {
   return (
-    <div>
-      <h1>Game</h1>
-       <button onClick={verifyLetter}>finalizar</button>
+    <div className="game">
+      <p className="points">
+          <span>Pontuaçao: 000</span>
+      </p>
+      <h1>Adivinhea palavra:</h1>
+        <h3 className="tip">
+          Dica sobre a palavra: <span>Dica...</span>
+        </h3>
+        <div className="wordContainer">
+          <span className="letter">a</span>
+          <span className="blankSquare"></span>
+        </div>
+        <div className="letterContainer">
+          <p>Tente advinhar uma letra da palavra:</p>
+           <form>
+            <input type="text" name="letter" maxLength="1" required/>
+             <button>Jogar</button>
+           </form>
+        </div>
+        <div className="wrongLetterContainer">
+           <p>Letras já ultilizadas</p>
+            <span>A, </span>
+            <span>b, </span>
+        </div>
     </div>
   )
 }
